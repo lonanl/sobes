6d3e9f675a39d31c53793a849c5e5376cf3d734b
llonan <99047777+lonanl@users.noreply.github.com>
2025-04-12 12:56:22 +0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <main>
-      <h3>Собеседование Metamentor</h3>
+      <h3>Собеседование Metamentor <br /></h3>
       <button onClick={() => setIsStopwatchVisible(!isStopwatchVisible)}>
         {isStopwatchVisible ? 'Закрыть и выключить' : 'Открыть'} секундомер
       </button>
--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -3,23 +3,23 @@ import { useState } from 'react'
 const Stopwatch = () => {
   const [seconds, setSeconds] = useState(0)
 
-  // 1.Починить секундомер и логи
-
-  // 2. Сделать чтобы при отключении т
+  // 1. Починить секундомер и логи. 
+  // 2. Сделать возможность сбрасывать таймер по той же кнопке. 
+  //    При отключении таймера в App.ts, таймер также должен выключаться.
 
   const start = () => {
     setSeconds(0)
-    setInterval (()=>{
+    setInterval(() => {
       setSeconds(seconds + 1)
       console.log(seconds)
-    },1000)
-}
-    
-  
+    }, 1000)
+  }
+
+
   return (
     <div>
       <br />
-      <button onClick={start}>Начать /Начать заново</button>
+      <button onClick={start}>Начать</button>
       <code>Время: {seconds}</code>
     </div>
   )
