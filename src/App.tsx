f57960b46daf33152c7c079e979b57d696f04c88
lonanl <llonan@bk.ru>
2025-04-18 16:03:59 +0300

    Немного поменял задачки

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react"
 import Stopwatch from "./components/Stopwatch"
 
 function App() {
-  const [isStopwatchVisible, setIsStopwatchVisible] = useState(true)
-
   return (
     <main>
       <h3>Собеседование Metamentor <br /></h3>
-      <button onClick={() => setIsStopwatchVisible(!isStopwatchVisible)}>
-        {isStopwatchVisible ? 'Закрыть и выключить' : 'Открыть'} секундомер
-      </button>
-      {isStopwatchVisible && <Stopwatch />}
+
+      <Stopwatch />
 
       {/* <Employees /> */}
     </main>
